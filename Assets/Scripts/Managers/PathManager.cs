bd8ebcf74fb7bf5dfefed246442652fd3fb23f7f
Quinton <quinncg.dev@gmail.com>
2024-06-27 21:49:20 -0400

    Upgrade to v0.3
    Built and uploaded v0.2

--- Assets/Scripts/Managers/PathManager.cs
+++ Assets/Scripts/Managers/PathManager.cs
@@ -1,8 +1,6 @@
 using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
